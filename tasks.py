4ac19c64b957d835fa9a4fb9a1f17245926db28c
Ubuntu <ubuntu@ip-172-31-1-72.eu-north-1.compute.internal>
2025-11-12 08:57:53 +0000

    feat: add config

--- tasks.py
+++ tasks.py
@@ -1,40 +1,72 @@
 from invoke import task
+import os
 
-COMPOSE_BASE = 'docker-compose -f development/docker-compose-base.yml'
-COMPOSE_DEV = f'{COMPOSE_BASE} -f development/docker-compose-dev.yml'
+COMPOSE_BASE = "docker-compose -f development/docker-compose-base.yml"
+COMPOSE_DEV = f"{COMPOSE_BASE} -f development/docker-compose-dev.yml"
 
 @task
 def build(c):
-    # Build nautobot and related containers
-    c.run(f'{COMPOSE_BASE} build nautobot')
+    """Build the plugin Docker image."""
+    python_ver = os.getenv("PYTHON_VER", "3.11")
+    nautobot_ver = os.getenv("NAUTOBOT_VER", "2.0.0")
+    dockerhub_user = os.getenv("DOCKERHUB_USER")
+    dockerhub_token = os.getenv("DOCKERHUB_TOKEN")
+    image_name = os.getenv("DOCKER_IMAGE", f"docker.io/{dockerhub_user}/nautobot-plugin-example:latest")
+
+    if not dockerhub_user or not dockerhub_token:
+        print("Error: DOCKERHUB_USER and DOCKERHUB_TOKEN environment variables must be set.")
+        return
+
+    build_cmd = (
+        f"docker build "
+        f"--build-arg PYTHON_VER={python_ver} "
+        f"--build-arg NAUTOBOT_VER={nautobot_ver} "
+        f"--build-arg DOCKERHUB_USER={dockerhub_user} "
+        f"--build-arg DOCKERHUB_TOKEN={dockerhub_token} "
+        f"-t {image_name} "
+        f"-f development/Dockerfile ."
+    )
+    c.run(build_cmd)
 
 @task
 def start(c):
-    c.run(f'{COMPOSE_DEV} up -d')
+    """Start the plugin environment."""
+    c.run(f"{COMPOSE_DEV} up -d")
 
 @task
 def stop(c):
-    c.run(f'{COMPOSE_DEV} down')
+    """Stop the plugin environment."""
+    c.run(f"{COMPOSE_DEV} down")
 
 @task
 def logs(c):
-    c.run(f'{COMPOSE_DEV} logs -f')
+    """Show logs of all containers."""
+    c.run(f"{COMPOSE_DEV} logs -f")
 
 @task
 def test(c):
-    c.run('pytest nautobot_plugin_example/tests')
-    
+    """Run tests with pytest."""
+    c.run("pytest nautobot_plugin_example/tests")
+
 @task
-def docker_login(c, registry='your-docker-registry.com', username=None, password=None):
+def docker_login(c, username=None, password=None, registry="docker.io"):
+    """Login to private Docker registry."""
     if not username or not password:
-        print('Usage: inv docker_login --username youruser --password yourpass')
+        print("Usage: inv docker_login --username <user> --password <pat>")
         return
-    c.run(f'docker login {registry} -u {username} -p {password}')
+    c.run(f"docker login {registry} -u {username} -p {password}")
 
 @task
-def docker_push(c, image='your-docker-registry.com/nautobot-plugin-example:latest'):
-    c.run(f'docker push {image}')
+def docker_push(c):
+    """Push image to private Docker registry."""
+    dockerhub_user = os.getenv("DOCKERHUB_USER")
+    image_name = os.getenv("DOCKER_IMAGE", f"docker.io/{dockerhub_user}/nautobot-plugin-example:latest")
+    c.run(f"docker push {image_name}")
 
 @task
-def docker_pull(c, image='your-docker-registry.com/nautobot-plugin-example:latest'):
-    c.run(f'docker pull {image}')
+def docker_pull(c):
+    """Pull image from private Docker registry."""
+    dockerhub_user = os.getenv("DOCKERHUB_USER")
+    image_name = os.getenv("DOCKER_IMAGE", f"docker.io/{dockerhub_user}/nautobot-plugin-example:latest")
+    c.run(f"docker pull {image_name}")
+
