ce1ba2d1759229b1f85979c15d5e9355b829365b
AkhilMohammed <52847762+AkhilMohammed@users.noreply.github.com>
2025-12-09 09:22:26 -0800

    Refactor plugin config to inherit from NautobotAppConfig

--- nautobot_plugin_example/apps.py
+++ nautobot_plugin_example/apps.py
@@ -1,21 +1,22 @@
-from nautobot.extras.plugins import PluginConfig
+from nautobot.apps import NautobotAppConfig
+
 
 __version__ = "1.0.0"
 
-class NautobotPluginExampleConfig(PluginConfig):
-    __module__ = "nautobot_plugin_example"  # Force root package module
+
+class NautobotPluginExampleConfig(NautobotAppConfig):
     name = "nautobot_plugin_example"
     verbose_name = "nautobot_example_plugin"
+    version = __version__
     description = "Custom dashboard for device and KPI health."
     author = "Akhil"
     author_email = "shaikMohammed.AkhilTaj@gmail.com"
+
     required_settings = []
     default_settings = {}
     min_version = "1.0.0"
     max_version = "3.0.0"
     caching_config = {}
-
-    urls = None  # disable URLs import
 
     def ready(self):
         super().ready()
